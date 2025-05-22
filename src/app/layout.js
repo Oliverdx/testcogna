7bd2a29b675a597c7df933a9fd1cfb7c9e63e5b5
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2025-05-21 23:07:17 -0300

    create the base layout for the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
+
+import Header from "@/components/Header";
+
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +25,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      >    
+        <div className="min-h-screen font-[family-name:var(--font-geist-sans)] text-white">
+          <Header />
+          <main>
+            {children}
+            </main>
+          <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+              Made with tons of code
+          </footer>
+        </div>
       </body>
     </html>
   );
